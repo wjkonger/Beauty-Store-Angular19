78878ed3298af9d56d794c69bd3585bf17512850
wjkonger <wjkonger@gmail.com>
2025-05-28 20:17:58 -0400

    Complete Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+// import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 export const routes: Routes = [
     {path: "", component: ProductListComponent},
     {path: "home", component: HomeComponent},
     {path: "product-list", component: ProductListComponent},
-    {path: "product-detail/:id", component: ProductDetailComponent},
-    {path: "about", component: ProductDetailComponent},
+    {path: "product-detail/:id", loadComponent: ()=>import("./product-detail/product-detail.component").then(c=>c.ProductDetailComponent)},
+    {path: "about", component: AboutComponent},
     {path: "contact", component: ContactComponent},
     {path: "**", component: ProductListComponent}
 ];
